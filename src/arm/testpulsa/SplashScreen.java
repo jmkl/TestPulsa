e496a7e842dafa01552567cd4bb9f93926886e03
damnedivan <tivanoedwin@androidminang.com>
2012-11-19 17:18:33 +0700

    - added splashscreen

---
+++ src/arm/testpulsa/SplashScreen.java
@@ -0,0 +1,32 @@
+package arm.testpulsa;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+// splash screen thread
+public class SplashScreen extends Activity {
+
+	private static final int SPLASH_DISPLAY_TIME = 1000;
+
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.splashscreen);
+
+	    new Handler().postDelayed(new Runnable() {
+
+	        public void run() {
+
+	            Intent mainIntent = new Intent(SplashScreen.this,
+	                    MainActivity.class);
+	            SplashScreen.this.startActivity(mainIntent);
+
+	            SplashScreen.this.finish();
+	            overridePendingTransition(R.anim.mainfadein,
+	                    R.anim.splashfadeout);
+	        }
+	    }, SPLASH_DISPLAY_TIME);
+	}
+
+}
