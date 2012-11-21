ee5f5f4a8904418f020095a2f4f862f03263a66a
dcsms <jimikill3d@gmail.com>
2012-11-21 12:54:48 +0700

    Base 64

--- src/arm/testpulsa/enkrip/EnkripDekrip.java
+++ src/arm/testpulsa/enkrip/EnkripDekrip.java
@@ -1,5 +1,7 @@
 package arm.testpulsa.enkrip;
-
+/*
+ * Maenkrip String Jadi Baso Planet
+ * */
 import android.util.Base64;
 
 public class EnkripDekrip {
@@ -7,12 +9,14 @@ public class EnkripDekrip {
 	public String enkode(String stringnankadienkode) {
 		String stringterenkrip = Base64.encodeToString(
 				stringnankadienkode.getBytes(), Base64.DEFAULT);
+		
 		return stringterenkrip;
 	}
 
 	public String dekode(String stringnankadidekode) {
 		String stringterdekrip = new String(Base64.decode(stringnankadidekode,
 				Base64.DEFAULT));
+		
 		return stringterdekrip;
 	}
 
