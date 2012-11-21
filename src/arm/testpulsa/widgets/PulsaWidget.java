4c08c47db764384a27cc4e31e18d9a0f7c87b85d
damnedivan <tivanoedwin@androidminang.com>
2012-11-21 23:35:55 +0700

    - added widget package and activity in order to instant action (Could not load widget)

---
+++ src/arm/testpulsa/widgets/PulsaWidget.java
@@ -0,0 +1,28 @@
+package arm.testpulsa.widgets;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import arm.testpulsa.R;
+
+public class PulsaWidget extends AppWidgetProvider{
+
+	public void onReceive(Context context, Intent intent)
+	{
+		String action = intent.getAction();
+		if (AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(action))
+		{
+			
+			RemoteViews views = new RemoteViews(context.getPackageName(),
+					R.layout.activity_main);
+
+			AppWidgetManager
+					.getInstance(context)
+					.updateAppWidget(
+							intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS),
+							views);
+		}
+	}
+}
