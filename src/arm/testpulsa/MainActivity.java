5a220e1dc12223275965e5a5376c96733172d125
damnedivan <tivanoedwin@androidminang.com>
2012-11-23 18:30:37 +0700

    - added FIXME annotation

--- src/arm/testpulsa/MainActivity.java
+++ src/arm/testpulsa/MainActivity.java
@@ -234,6 +234,8 @@ public class MainActivity extends Activity implements TextWatcher {
 		sms.sendTextMessage(phoneNo, null, message, sentPI, deliveredPI);
 	}
 
+	// FIXME some problems maybe shown up because of a change in the sendSMS
+	
 	private class SendButtonOnClick implements OnClickListener {
 
 		public void onClick(View v) {
