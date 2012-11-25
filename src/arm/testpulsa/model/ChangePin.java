842a44c17a22be5121561c5d6a8449704bdafd38
damnedivan <tivanoedwin@androidminang.com>
2012-11-25 11:32:48 +0700

    - added ChangePin options

---
+++ src/arm/testpulsa/model/ChangePin.java
@@ -0,0 +1,136 @@
+package arm.testpulsa.model;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import arm.testpulsa.R;
+
+public class ChangePin extends Activity implements TextWatcher {
+
+	EditText txtPinOld, txtPinNew;
+	Button btnChangePin;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.change_pin);
+
+		// find view
+		txtPinOld = (EditText) findViewById(R.id.textPinOld);
+		txtPinNew = (EditText) findViewById(R.id.textPinNew);
+		btnChangePin = (Button) findViewById(R.id.btn_changePin);
+
+		// set listener
+		txtPinOld.addTextChangedListener(this);
+		txtPinNew.addTextChangedListener(this);
+		btnChangePin.setEnabled(false);
+		btnChangePin.setOnClickListener(new ChangeButtonOnClick());
+
+	}
+
+	@Override
+	public void afterTextChanged(Editable arg0) {
+		// nothing to do here
+
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+		// nothing to do here
+
+	}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		// nothing to do here
+
+	}
+
+	private class ChangeButtonOnClick implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			String pinOld, pinNew;
+			pinOld = txtPinOld.getText().toString();
+			pinNew = txtPinNew.getText().toString();
+			
+			final String smsMessage = String.format("s.%s.%s", pinOld, pinNew );
+			
+				sendSMS("+6297792021743", smsMessage);
+		}
+	}
+
+	public void sendSMS(String phoneNo, String message) {
+		String SENT = "SMS_SENT";
+		String DELIVERED = "SMS_DELIVERED";
+
+		PendingIntent sentPI = PendingIntent.getBroadcast(
+				getApplicationContext(), 0, new Intent(SENT), 0);
+
+		PendingIntent deliveredPI = PendingIntent.getBroadcast(
+				getApplicationContext(), 0, new Intent(DELIVERED), 0);
+
+		// ---when the SMS has been sent---
+		registerReceiver(new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context arg0, Intent arg1) {
+				switch (getResultCode()) {
+				case Activity.RESULT_OK:
+					Toast.makeText(getBaseContext(), "Request Sent",
+							Toast.LENGTH_SHORT).show();
+					break;
+				case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+					Toast.makeText(getBaseContext(), "Generic failure",
+							Toast.LENGTH_SHORT).show();
+					break;
+				case SmsManager.RESULT_ERROR_NO_SERVICE:
+					Toast.makeText(getBaseContext(), "No service",
+							Toast.LENGTH_SHORT).show();
+					break;
+				case SmsManager.RESULT_ERROR_NULL_PDU:
+					Toast.makeText(getBaseContext(), "Null PDU",
+							Toast.LENGTH_SHORT).show();
+					break;
+				case SmsManager.RESULT_ERROR_RADIO_OFF:
+					Toast.makeText(getBaseContext(), "Radio off",
+							Toast.LENGTH_SHORT).show();
+					break;
+				}
+			}
+		}, new IntentFilter(SENT));
+
+		// ---when the SMS has been delivered---
+		registerReceiver(new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context arg0, Intent arg1) {
+				switch (getResultCode()) {
+				case Activity.RESULT_OK:
+					Toast.makeText(getBaseContext(), "SMS terkirim",
+							Toast.LENGTH_SHORT).show();
+					break;
+				case Activity.RESULT_CANCELED:
+					Toast.makeText(getBaseContext(), "SMS tidak terkirim",
+							Toast.LENGTH_SHORT).show();
+					break;
+				}
+			}
+		}, new IntentFilter(DELIVERED));
+
+		SmsManager sms = SmsManager.getDefault();
+		sms.sendTextMessage(phoneNo, null, message, sentPI, deliveredPI);
+	}
+
+}
