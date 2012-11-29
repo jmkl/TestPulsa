aa82ed707049389b7d94a4ee60505a3ae4e21896
adrianbabame <adrianbabame@gmail.com>
2012-11-29 21:56:58 +0700

    Moving to utils

--- src/arm/testpulsa/ArmPulsaAddressMalformedException.java
+++
@@ -1,9 +0,0 @@
-package arm.testpulsa;
-
-public class ArmPulsaAddressMalformedException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public ArmPulsaAddressMalformedException(String msg) {
-		super(msg);
-	}
-}
