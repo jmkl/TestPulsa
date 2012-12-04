dbfee2b0cf1a96e0d4d0065f961d24acc592ddba
adrianbabame <adrianbabame@beam-PC>
2012-12-04 17:20:24 +0700

    Add: Tab CekTransaksi Remove: implementation of TextWatcher

---
+++ src/arm/testpulsa/ui/BaseSherlockFragment.java
@@ -0,0 +1,25 @@
+package arm.testpulsa.ui;
+
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import arm.testpulsa.TestPulsaConfiguration;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+public abstract class BaseSherlockFragment extends SherlockFragment {
+	protected TestPulsaConfiguration mConfig;
+	protected String userPin;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		mConfig = new TestPulsaConfiguration(
+				PreferenceManager
+						.getDefaultSharedPreferences(getSherlockActivity()));
+		userPin = mConfig.userPin;
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+}
